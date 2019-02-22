13b4c5444d401e5dfd5c76bba6d497b1e0580d21
tirthrajchauhan <tirthraj456@gmail.com>
2019-02-21 22:03:44 -0500

    Added spin button functionality

--- Slot-Machine-MidTerm/GameScene.swift
+++ Slot-Machine-MidTerm/GameScene.swift
@@ -21,6 +21,10 @@ class GameScene: SKScene {
     var backgroundbg: Background?
     var reset: Reset?
     var quit: Quit?
+    var spinImage1node:SKSpriteNode!
+    var spinImage2node:SKSpriteNode!
+    var spinImage3node:SKSpriteNode!
+    var textures = [SKTexture]()
 
     override func didMove(to view: SKView) {
      
@@ -35,6 +39,7 @@ class GameScene: SKScene {
      
         spinButton = SpinButton()
         spinButton?.position = CGPoint(x: 0.0, y: -500.0)
+        spinButton?.name = "spinButton"
         addChild(spinButton!)
         
         // add reset to scene
@@ -48,10 +53,54 @@ class GameScene: SKScene {
         quit = Quit()
         quit?.position = CGPoint(x: -200.0, y: -500.0)
         addChild(quit!)
+        
+        textures.append(SKTexture(imageNamed: "Cherry"))
+        
+        textures.append(SKTexture(imageNamed: "Apple"))
+        
+        textures.append(SKTexture(imageNamed: "Grape"))
+        
+        textures.append(SKTexture(imageNamed: "Kiwi"))
+        
+        textures.append(SKTexture(imageNamed: "Lemon"))
+        
+        textures.append(SKTexture(imageNamed: "Banana"))
+        
+        spinImage1node = self.childNode(withName: "spinImage1") as? SKSpriteNode
+        spinImage2node = self.childNode(withName: "spinImage2") as? SKSpriteNode
+        spinImage3node = self.childNode(withName: "spinImage3") as? SKSpriteNode
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+             let nodesArray = self.nodes(at: location)
+             if nodesArray.first?.name == "spinButton"{
+                let rand = Int(arc4random_uniform(UInt32(textures.count)))
+            
+                let rand2 = Int(arc4random_uniform(UInt32(textures.count)))
+                
+                let rand3 = Int(arc4random_uniform(UInt32(textures.count)))
+                
+                let texture = textures[rand] as SKTexture
+                
+                let texture2 = textures[rand2] as SKTexture
+                
+                let texture3 = textures[rand3] as SKTexture
+                
+                spinImage1node.texture = texture
+                
+                spinImage2node.texture = texture2
+                
+                spinImage3node.texture = texture3
+                
+      }
+            
+        }
     }
     
     
-  
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
